00303e0a0115c6d563526b3d43c8f23b61a33365
vereinpferd.de <gaal.bertold@gmail.com>
2026-05-17 04:04:23 +0200

    Alap Fő verzió init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Online Szavazási Platform</h1>
+
+      <p>
+        Üdvözöllek az online szavazási rendszerben!
+      </p>
+
+      <button>Szavazások megtekintése</button>
+    </div>
+  )
+}
+
+export default App
